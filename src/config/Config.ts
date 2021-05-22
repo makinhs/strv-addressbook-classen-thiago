b4d2da12c0c4b3c30be6ac1c8c43c32bb04c077a
Thiago Classen <tgclassen.dev@gmail.com>
2021-05-22 20:46:09 -0300

    argon2 working

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import * as argon2 from 'argon2';
 
 dotenv.config();
 
@@ -12,9 +13,13 @@ const SERVER = {
 
 const ENV = process.env.NODE_ENV || 'development';
 
+/** Follwing OWASP guidelines https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html */
+const argonConfig = { type: argon2.argon2id, memoryCost: 2 ** 16, hashLength: 50, parallelism: 2 };
+
 const config = {
     server: SERVER,
-    env: ENV
+    env: ENV,
+    argon: argonConfig
 };
 
-export default config;
+export { config };

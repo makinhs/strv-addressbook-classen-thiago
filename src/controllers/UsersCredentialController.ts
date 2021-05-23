119f201f59beeae3ab20603e28680245209b91b8
Thiago Classen <tgclassen.dev@gmail.com>
2021-05-22 22:12:50 -0300

    user auth working

--- src/controllers/UsersCredentialController.ts
+++ src/controllers/UsersCredentialController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import { logger } from '../config/Logger';
 import { User } from '../db/models/user';
 
 class UsersCredentialController {
@@ -7,12 +8,14 @@ class UsersCredentialController {
         const repository = getRepository(User),
             { email, password } = req.body;
 
+        logger.info('checking if the user already exists.');
         const userExists = await repository.findOne({ where: { email } });
 
         if (userExists) {
             return res.status(409).json({ error: 'User already registered.' });
         }
 
+        logger.info('saving new user data');
         const user = repository.create({ email, password });
         await repository.save(user);
 

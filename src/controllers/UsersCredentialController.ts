c18efcacc07d32ea42186f8e01ae90022420ceeb
Thiago Classen <tgclassen.dev@gmail.com>
2021-05-23 14:59:04 -0300

    refactor user controller

--- src/controllers/UsersCredentialController.ts
+++ src/controllers/UsersCredentialController.ts
@@ -1,28 +1,22 @@
-import { NextFunction, Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { Request, Response } from 'express';
 import { logger } from '../config/Logger';
 import { User } from '../db/models/user';
-import * as jwt from 'jsonwebtoken';
-import { config } from '../config/Config';
+import { userService } from '../services/User.service';
+import { authService } from '../services/Auth.service';
 
 class UsersCredentialController {
     async store(req: Request, res: Response) {
-        const repository = getRepository(User),
-            { email, password } = req.body;
+        const { email, password } = req.body;
 
-        logger.info('checking if the user already exists.');
-        const userExists = await repository.findOne({ where: { email } });
-
-        if (userExists) {
+        let user: User;
+        try {
+            user = await userService.registerNewUser(email, password);
+        } catch (err) {
             return res.status(409).json({ error: 'User already registered.' });
         }
 
-        logger.info('saving new user data');
-        const user = repository.create({ email, password });
-        await repository.save(user);
-
         logger.info('signing user jwt');
-        const jwtToken = jwt.sign({ id: user.id }, config.jwtSecret, { expiresIn: '1d' });
+        const jwtToken = authService.signJWT(user.id);
 
         res.status(201).json({ token: jwtToken });
     }
--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { logger } from '../config/Logger';
 import { User } from '../db/models/user';
 
 class UserService {
@@ -6,6 +7,22 @@ class UserService {
     async getUserByEmail(email: string) {
         const repository = getRepository(User);
         return await repository.findOne({ where: { email } });
+    }
+    async registerNewUser(email: string, password: string) {
+        const repository = getRepository(User);
+
+        logger.info('checking if the user already exists.');
+        const userExists = await repository.findOne({ where: { email } });
+
+        if (userExists) {
+            throw Error('User already registered.');
+        }
+
+        logger.info('saving new user data');
+        const user = repository.create({ email, password });
+        await repository.save(user);
+
+        return user;
     }
 }
 
